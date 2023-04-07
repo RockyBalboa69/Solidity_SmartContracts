cc08f9ba55a43dec39f62cf5568ee4e304fbc5f0
Abhay Sharma <75359031+RockyBalboa69@users.noreply.github.com>
2023-04-08 01:25:33 +0530

    local variables basics

---
+++ local_var.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <0.9.0;
+contract local 
+{
+    function setage() pure public returns(uint)
+    {
+        uint age=10; 
+        return age;
+
+
+    }
+}
